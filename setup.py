992bfe19bb9b8f5fb4480f01f98aa031bffef692
Michał Startek <michal.startek@mimuw.edu.pl>
2025-02-13 14:56:23 +0100

    Fix Windows support

--- setup.py
+++ setup.py
@@ -5,19 +5,22 @@ from setuptools import setup
 __version__ = open("pyproject.toml").read().split('version = "')[1].split('"')[0]
 
 import os
+sources = ["src/pylmcf.cpp", "lemon/base.cc"]
+
 if os.name == 'nt':
-    threads = ("LEMON_USE_WIN32_THREADS", 1)
+    sources += ["lemon/bits/windows.cc"]
+    os_flags = [("WIN32", 1), ("LEMON_USE_WIN32_THREADS", 1)]
 else:
-    threads = ("LEMON_USE_PTHREAD", 1)
+    os_flags = [("LEMON_USE_PTHREAD", 1)]
 
 
 ext_modules = [
     Pybind11Extension(
         "pylmcf_cpp",
-        ["src/pylmcf.cpp"],
+        sources,
         include_dirs=["src", "."],
-        define_macros=[("LMCF_VERSION", __version__), threads],
-        cxx_std=23,
+        define_macros=[("LMCF_VERSION", __version__)] + os_flags,
+        cxx_std=20,
     )
 ]
 
