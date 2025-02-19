9ac9ce7a97c73d71f1a13a5ec926532121bc4e01
Michał Startek <michal.startek@mimuw.edu.pl>
2025-02-19 11:43:16 +0100

    Pytest

--- tests/test_graph.py
+++ tests/test_graph.py
@@ -1,8 +1,11 @@
 from pylmcf_cpp import Graph
 import numpy as np
 
-G = Graph(3, np.array([0, 0, 1]), np.array([1, 2, 2]), np.array([1, 3, 5]))
-G.set_edge_capacities(np.array([1, 2, 3]))
-G.set_node_supply(np.array([5, 0, -5]))
-G.solve()
-print(G.result(), G.total_cost())
+
+def test_graph_simple():
+    G = Graph(3, np.array([0, 0, 1]), np.array([1, 2, 2]), np.array([1, 3, 5]))
+    G.set_edge_capacities(np.array([1, 2, 3]))
+    G.set_node_supply(np.array([5, 0, -5]))
+    G.solve()
+    assert all(G.result() == np.array([1, 2, 1]))
+    assert G.total_cost() == 12
