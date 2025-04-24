Bugfix; scaling factor in cost

pylmcf/wasserstein.py
@@ -372,7 +372,7 @@ class WassersteinNetwork:
         self.theoretical_spectra = theoretical_spectra
         if distance_limit is None:
             distance_limit = max((t.distance_limit() for t in trashes), default=np.inf)
-        G = DecompositableFlowGraph()
+        G = FlowGraph()
         self.G = G
         self.source = G.source
         self.sink = G.sink

tests/test_decompositable.py
@@ -6,14 +6,17 @@ import numpy as np
 
 def test_simple():
     S1 = Spectrum(np.array([[1, 2, 30]]), np.array([1, 4, 3]))
-    S2 = Spectrum(np.array([[1, 4, 30, 31]]), np.array([1, 4, 1, 2]))
+    S2 = Spectrum(np.array([[1, 4, 30, 31]]), np.array([1, 1, 1, 1]))
+    S3 = Spectrum(np.array([[3, 4, 32]]), np.array([1, 1, 3]))
 
     G = DecompositableFlowGraph()
     G.add_empirical_spectrum(S1)
     G.add_theoretical_spectrum(S2, lambda x, y: np.linalg.norm(x - y, axis=0), 5)
+    G.add_theoretical_spectrum(S3, lambda x, y: np.linalg.norm(x - y, axis=0), 5)
     G.build([TrashFactorySimple(10)])
     #G.show()
-    print(G.set_point([1]))
+    print(G.set_point([1, 1]))
+
     #for fr_gr in G.fragment_graphs:
     #    fr_gr.show()
 

tests/test_multidim.py
@@ -1,6 +1,8 @@
 from pylmcf.wasserstein import WassersteinSolver
 from pylmcf.spectrum import Spectrum, Spectrum_1D
-from pylmcf.trashes import SimpleTrash
+from pylmcf.trashes import SimpleTrash, TrashFactorySimple
+from pylmcf.graph import DecompositableFlowGraph
+from pylmcf.solver import DeconvolutionSolver
 import numpy as np
 
 
@@ -23,9 +25,29 @@ def test_2d():
         [SimpleTrash(1000000)],
         costs_scaling=1000
     )
-    print(solver.run())
+    #print(solver.run())
     assert solver.run() == 1.414
 
+    # new algo
+    DG = DecompositableFlowGraph()
+    DG.add_empirical_spectrum(s1)
+    DG.add_theoretical_spectrum(s2, lambda x, y: 1000*np.linalg.norm(x - y, axis=0), 5000)
+    DG.build([TrashFactorySimple(1000000)])
+    #print(DG.set_point([1]))
+    assert DG.set_point([1]) == 1414
+
+    # new solver
+    solver = DeconvolutionSolver(
+        s1,
+        [s2],
+        distance_function=lambda x, y: np.linalg.norm(x - y, axis=0),
+        max_distance=5,
+        trash_cost=1000000,
+        scale_factor=1000
+    )
+    ret = solver.set_point([1])
+    solver.DG.subgraphs[0].show()
+    assert ret == 1.414
 
 
 if __name__ == "__main__":