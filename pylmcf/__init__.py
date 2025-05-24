ec58cafc7ccfcdbbaca4713f0cbf0b055230c6c5
Michał Startek <michal.startek@mimuw.edu.pl>
2025-05-24 08:09:42 +0200

    Fix paths

--- pylmcf/__init__.py
+++ pylmcf/__init__.py
@@ -2,6 +2,13 @@ import pylmcf_cpp
 from .wasserstein import *
 from .spectrum import *
 
+import os
 import importlib.metadata
 
 __version__ = importlib.metadata.version("pylmcf")
+
+def include() -> str:
+    """
+    Returns the include path for the C++ library
+    """
+    return os.path.join(os.path.abspath(os.path.dirname(__file__)), "include")
