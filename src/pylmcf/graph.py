408eb4b03010bb74b813c77fd3cd4fc9a80e09a3
Michał Startek <michal.startek@mimuw.edu.pl>
2025-09-10 09:55:08 +0200

    Some cleanup

--- src/pylmcf/graph.py
+++ src/pylmcf/graph.py
@@ -1,10 +1,4 @@
-from dataclasses import dataclass
-from typing import Union
-from abc import ABC
-from pprint import pprint
 import numpy as np
-import networkx as nx
-from tqdm import tqdm
 
 from pylmcf.pylmcf_cpp import CGraph
 
@@ -16,6 +10,7 @@ class Graph(CGraph):
         """
         Convert the C++ subgraph to a NetworkX graph.
         """
+        import networkx as nx
         nx_graph = nx.DiGraph()
         for node_id in range(self.no_nodes()):
             nx_graph.add_node(node_id)
@@ -30,6 +25,7 @@ class Graph(CGraph):
         """
         Show the C++ subgraph as a NetworkX graph.
         """
+        import networkx as nx
         from matplotlib import pyplot as plt
         nx_graph = self.as_nx()
         plt.figure(figsize=(8, 6))
