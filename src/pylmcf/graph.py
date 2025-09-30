8a752e567d492ca44f2bf21fafdd3bab33ff913f
Michał Startek <michal.startek@mimuw.edu.pl>
2025-09-30 19:01:04 +0200

    Some type annotations

--- src/pylmcf/graph.py
+++ src/pylmcf/graph.py
@@ -3,10 +3,10 @@ import numpy as np
 from pylmcf.pylmcf_cpp import CGraph
 
 class Graph(CGraph):
-    def __init__(self, no_nodes: int, edge_starts: np.ndarray, edge_ends: np.ndarray):
+    def __init__(self, no_nodes: int, edge_starts: np.ndarray, edge_ends: np.ndarray) -> None:
         super().__init__(no_nodes, edge_starts, edge_ends)
 
-    def as_nx(self):
+    def as_nx(self) -> "nx.DiGraph":
         """
         Convert the C++ subgraph to a NetworkX graph.
         """
@@ -21,7 +21,7 @@ class Graph(CGraph):
             )
         return nx_graph
 
-    def show(self):
+    def show(self) -> None:
         """
         Show the C++ subgraph as a NetworkX graph.
         """
@@ -31,9 +31,3 @@ class Graph(CGraph):
         plt.figure(figsize=(8, 6))
         nx.draw(nx_graph, with_labels=True)
         plt.show()
-        # pos = nx.multipartite_layout(nx_graph, subset_key="layer")
-        # edge_labels = nx.get_edge_attributes(nx_graph, "label")
-        # nx.draw(nx_graph, with_labels=True, pos=pos)
-        # nx.draw_networkx_edge_labels(nx_graph, pos=pos, edge_labels=edge_labels)
-        # plt.show()
-
