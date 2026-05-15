965f9752d4270251b7957df28dc93dbd2408dba8
Michal Startek <michal.startek@mimuw.edu.pl>
2026-05-15 19:26:49 +0200

    Faster devel rebuilds

--- reinstall.sh
+++ reinstall.sh
@@ -1,4 +1,4 @@
 pip uninstall pylmcf -y
 rm -rf build *.so pylmcf.egg-info
-VERBOSE=1 pip install --no-deps -v -e .
+SKBUILD_BUILD_DIR=_skbuild_$(hostname -s) VERBOSE=1 pip install --no-deps -v -e . --no-build-isolation
 
